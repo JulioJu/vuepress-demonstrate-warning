ae80a48d485bb946348faa0d72bb130d70f44bdf
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-04-11 16:33:21 +0200

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,6 @@
+const path = require('path');
+
+module.exports = {
+  theme: path.resolve(__dirname, '../../theme'),
+};
+
---
+++ theme/index.ts
@@ -0,0 +1,5 @@
+const path = require('path');
+
+module.exports = {
+  clientAppRootComponentFiles: [path.resolve(__dirname, 'Test.vue')],
+};
